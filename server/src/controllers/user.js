a4505c79f33f22ea01d9b040bcdf60626a74e0fa
julietvt <freshitpt@gmail.com>
2020-08-01 12:20:38 +0300

    update user controllers

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -15,6 +15,39 @@ export async function createUser(req,res,next) {
 
 export async function updateUserByPk(req,res,next) {
     try{
+        const [RowsCount, Rows] = await User.update(req.body,{
+            where: {
+                id: req.params.userId,
+            },
+            returning: true,
+        });
+        if(RowsCount){
+            const data = Rows[0].get();
+            delete data.password;
+            return res.send(data);
+        }
+        next('User info not found!');
+    }catch(e){ next(e);}
+}
 
+export async function getUserByPk(req,res,next) {
+    try{
+        const foundUser = await User.findByPk(req.params.userId,{
+            attributes:{
+                exclude: ['password']
+            }
+        });
+        if(foundUser) { return res.send(foundUser);}
+        next('User not found!!!');
+    }catch(e){ next(e);}
+}
+
+export async function deleteUserByPk(req,res,next) {
+    try{
+        const deletedRowsCount = await User.destroy({
+                where: { id: req.params.userId }
+            } );
+        if(deletedRowsCount) { res.send(`${deletedRowsCount}`)};
+        next('User not found!!!');
     }catch(e){ next(e);}
 }
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {createUser, updateUserByPk} from "../controllers/user";
-import {validateUserOnUpdate} from "../middlewares/user/validateUser";
+import {createUser, updateUserByPk, deleteUserByPk, getUserByPk} from "../controllers/user";
+import {validateUserOnUpdate, validateUserOnCreate} from "../middlewares/user/validateUser";
 const userRouter = express.Router();
 //userRouter.post('/', createUser);
-userRouter.patch('/',validateUserOnUpdate, updateUserByPk);
+userRouter.patch('/:userId',validateUserOnUpdate, updateUserByPk);
+userRouter.get('/:userId',getUserByPk);
+userRouter.delete('/:userId',deleteUserByPk);
 export default userRouter;
 
