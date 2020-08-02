1a92070106e71586c1c2293f6e132c9d04b79e42
julietvt <freshitpt@gmail.com>
2020-08-02 12:05:04 +0300

    update user.router

--- server/src/middlewares/validation/createValidationMW.js
+++ server/src/middlewares/validation/createValidationMW.js
@@ -1,17 +1,17 @@
-/*
-import userSchema from '../../utils/validation/user';
-function createUserValidationMV(isCreateMode=true) {
-    return async(req,res,next) => {
-        try {
-            req.body = await userSchema.validateAsync(req.body, {
-                context: {
-                    isCreateMode: true,
-                }
-            });
-            next();
-        } catch(e){ next(e); }}}
 
-export const validateUserOnCreate = createUserValidationMV();
-export const validateUserOnUpdate = createUserValidationMV(false);
-
- */
+export default function createValidationMW(schema) {
+    return (isCreateMode = true) => {
+        return async (req, res, next) => {
+            try {
+                req.body = await schema.validateAsync(req.body, {
+                    context: {
+                        isCreateMode,
+                    }
+                });
+                next();
+            } catch (e) {
+                next(e);
+            }
+        };
+    };
+}
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {createUser, updateUserByPk, deleteUserByPk, getUserByPk} from "../controllers/user.js";
-import {validateUserOnUpdate, validateUserOnCreate} from "../middlewares/user/validateUser.js";
+//import {validateUserOnUpdate, validateUserOnCreate} from "../middlewares/user/validateUser.js";
+import createValidationMW from "../middlewares/validation/createValidationMW";
+import schemas from '../utils/validation';
 const userRouter = express.Router();
-userRouter.post('/', validateUserOnCreate, createUser);
-userRouter.patch('/:userId',validateUserOnUpdate, updateUserByPk);
+userRouter.post('/', createValidationMW(schemas.userSchema)(), createUser);
+userRouter.patch('/:userId',createValidationMW(schemas.userSchema)(false), updateUserByPk);
 userRouter.get('/:userId',getUserByPk);
 userRouter.delete('/:userId',deleteUserByPk);
 export default userRouter;
